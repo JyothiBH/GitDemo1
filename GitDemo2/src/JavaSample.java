31e790ea688b178192059cda2041d18919a5df42
JyothiBH <jyothibhalimani@gmail.com>
2020-02-11 07:11:35 -0500

    Updates with testcase1 method

--- GitDemo2/src/JavaSample.java
+++ GitDemo2/src/JavaSample.java
@@ -4,6 +4,13 @@ public class JavaSample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Code written by Jyothi
+		testcase1();
+		
+	}
+	
+	public static void testcase1()
+	{
+		
 	}
 
 }
