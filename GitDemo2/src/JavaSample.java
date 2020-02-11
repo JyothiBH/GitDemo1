fd5dbeb152172f353bdf83c219f507a0d4461876
JyothiBH <60921344+JyothiBH@users.noreply.github.com>
2020-02-11 07:18:06 -0500

    Update JavaSample.java

--- GitDemo2/src/JavaSample.java
+++ GitDemo2/src/JavaSample.java
@@ -5,10 +5,16 @@ public class JavaSample {
 		// TODO Auto-generated method stub
 		//Code written by Jyothi
 		testcase1();
+		testcase2();
 		
 	}
 	
 	public static void testcase1()
+	{
+		
+	}
+	
+	public static void testcase2()
 	{
 		
 	}
